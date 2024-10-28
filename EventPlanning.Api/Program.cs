a666ee91b822a2734a3b98acdf24f53fac586221
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-10-29 00:49:05 +0300

    Removed JWT

--- EventPlanning.Api/Program.cs
+++ EventPlanning.Api/Program.cs
@@ -5,19 +5,19 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = AuthOptions.ISSUER,
-        ValidateAudience = true,
-        ValidAudience = AuthOptions.AUDIENCE,
-        ValidateLifetime = true,
-        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-        ValidateIssuerSigningKey = true,
-    };
-});
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+//{
+//    options.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuer = true,
+//        ValidIssuer = AuthOptions.ISSUER,
+//        ValidateAudience = true,
+//        ValidAudience = AuthOptions.AUDIENCE,
+//        ValidateLifetime = true,
+//        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+//        ValidateIssuerSigningKey = true,
+//    };
+//});
 
 var app = builder.Build();
 
