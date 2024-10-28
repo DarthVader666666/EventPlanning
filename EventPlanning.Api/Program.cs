5d2ae76f5ab2a72758931b458eff474573ac8191
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-10-29 00:40:45 +0300

    Added JWT

--- EventPlanning.Api/Program.cs
+++ EventPlanning.Api/Program.cs
@@ -1,4 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using static EventPlanning.Server.Controllers.AuthorizationController;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = AuthOptions.ISSUER,
+        ValidateAudience = true,
+        ValidAudience = AuthOptions.AUDIENCE,
+        ValidateLifetime = true,
+        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+        ValidateIssuerSigningKey = true,
+    };
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
