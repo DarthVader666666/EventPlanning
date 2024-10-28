f200d202612438e00da4062892c5abbf690d1a86
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-10-29 01:49:04 +0300

    Refactored Program

--- EventPlanning.Api/Program.cs
+++ EventPlanning.Api/Program.cs
@@ -1,25 +1,30 @@
 using EventPlanning.Api.Configurations;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthorization();
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-//{
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidIssuer = AuthOptions.ISSUER,
-//        ValidateAudience = true,
-//        ValidAudience = AuthOptions.AUDIENCE,
-//        ValidateLifetime = true,
-//        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-//        ValidateIssuerSigningKey = true,
-//    };
-//});
+//builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = "MyAuthServer",
+        ValidateAudience = true,
+        ValidAudience = "MyAuthClient",
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysupersecret_secretsecretsecretkey!123")),
+        ValidateIssuerSigningKey = true,
+    };
+});
 
 var app = builder.Build();
+
+app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapGet("/", () => "Hello World!");
 
