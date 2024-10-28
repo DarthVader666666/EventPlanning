3e495d224ddf581d0e0ae05c209a84f1b6f1e098
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-10-29 00:19:28 +0300

    Added Bll proj

---
+++ EventPlanning.Bll/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace EventPlanning.Bll.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity?>> GetListAsync();
+        Task<TEntity?> GetAsync(object? id);
+        Task<TEntity?> CreateAsync(TEntity item);
+        Task<TEntity?> UpdateAsync(TEntity item);
+        Task<TEntity?> DeleteAsync(object? id);
+        Task<bool> ExistsAsync(TEntity item);
+    }
+}
